c1ef619a8c67828f17fcd082c13774409a51e052
Jess Ogwu <ogwujessica2222@gmail.com>
2022-12-04 23:10:12 +0100

    EDA and feature engineering

---
+++ model_building/variables.py
@@ -0,0 +1,89 @@
+EMPLOYMENT=['Employed full-time',
+'Independent contractor, freelancer, or self-employed',
+'Employed part-time',
+'Student, full-time',
+'Not employed, but looking for work']
+
+COUNTRY=[
+'United States of America',                               
+'India',                                                    
+'Germany',                                                  
+'United Kingdom of Great Britain and Northern Ireland',   
+'Canada',                                                  
+'France',                                                   
+'Brazil',                                                   
+'Poland',                                                   
+'Netherlands',                                              
+'Italy',                                                    
+'Spain',                                                    
+'Russian Federation',                                       
+'Australia' 
+]
+
+EDLEVEL=[
+"Bachelor’s degree (B.A., B.S., B.Eng., etc.)",                                         
+"Master’s degree (M.A., M.S., M.Eng., MBA, etc.)",                                       
+'Some college/university study without earning a degree',                                 
+'Secondary school (e.g. American high school, German Realschule or Gymnasium, etc.)',    
+'Other Degree(Associate or Professional)',                                                
+'Doctoral Degree',                                                                        
+'Something else'
+]
+
+DEVTYPE=[
+'Developer, full-stack',                            
+'Developer, front-end',                              
+'Developer, mobile',                                 
+'Developer, back-end',                               
+'Developer, desktop or enterprise applications',     
+'Not entirely sure of my devtype',                   
+'Engineer, data',                                    
+'Data scientist or machine learning specialist',      
+'Developer, embedded applications or devices',        
+'DevOps specialist',                                  
+'Developer, QA or test',                              
+'Engineering manager',                                
+'Academic researcher',                                
+'Developer, game or graphics',                        
+'Senior Executive (C-Suite, VP, etc.)',               
+'Data or business analyst',                            
+'Scientist',                                           
+'Student',                                             
+'Engineer, site reliability',                          
+'Database administrator',                              
+'Product manager',                                     
+'Designer',                                            
+'Educator',                                            
+'System administrator',                                
+'Marketing or sales professional' 
+]
+
+ORGSIZE=['20 to 99 employees',                                    
+'100 to 499 employees',                                  
+'10,000 or more employees',                             
+'1,000 to 4,999 employees',                              
+'2 to 9 employees',                                      
+'10 to 19 employees',                                    
+'500 to 999 employees',                                  
+'I don’t know',                                          
+'Just me - I am a freelancer, sole proprietor, etc.',    
+'5,000 to 9,999 employees' 
+]
+
+
+AGE=['25-34 years old',       
+'35-44 years old',        
+'18-24 years old',        
+'45-54 years old',        
+'55-64 years old',        
+'Prefer not to say',       
+'Under 18 years old',      
+'65 years or older'
+]
+
+GENDER=[ 
+'Man',                             
+'Woman',                             
+'Prefer not to state my gender',      
+'Other gender forms'    
+]
